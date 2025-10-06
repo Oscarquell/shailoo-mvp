2420572ba2548c7e4713278052079313a52b1b47
oscarquell <oscarquell96@gmail.com>
2025-10-06 10:45:26 +0600

    добавил кнопку

--- src/pages/authorization/authorization.jsx
+++ src/pages/authorization/authorization.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './authorization.css'
 import logo from '../../assets/logo/logo_emgek.png'
 import {Button, TextField} from "@mui/material";
+import ButtonComponent from "../../components/button/button";
 
 
 const Authorization = () => {
@@ -18,7 +19,12 @@ const Authorization = () => {
                     <img src={logo} alt="" className="authorization-logo-imng"/>
                     <TextField id="outlined-basic" label="Логин" variant="outlined" />
                     <TextField id="outlined-password-input"  label="Пароль" type="password" autoComplete="current-password" />
-                    <Button variant="outlined" onClick={test}>Войти</Button>
+                    {/*<Button variant="outlined" onClick={test}>Войти</Button>*/}
+                    <ButtonComponent
+                        variant="outlined"
+                        onClick={test}
+                        text={'Войти'}
+                    />
                 </div>
             </div>
         </div>
